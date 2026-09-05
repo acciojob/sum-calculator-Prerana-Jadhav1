47e0affbf21a1f80f908ca89dd7d3f7b98618ad2
Prerana-Jadhav1 <preranajb5@gmail.com>
2026-09-05 13:29:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [numbers, setNumbers] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    const calculateSum = async () => {
+      const total = numbers.reduce((acc, num) => acc + num, 0);
+      setSum(total);
+    };
+    calculateSum();
+  }, [numbers]);
+
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    setNumbers([...numbers, isNaN(value) ? 0 : value]);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Sum Calculator</h1>
+      <input type="number" onChange={handleChange} />
+      <p>Sum: {sum}</p>
     </div>
   )
 }
